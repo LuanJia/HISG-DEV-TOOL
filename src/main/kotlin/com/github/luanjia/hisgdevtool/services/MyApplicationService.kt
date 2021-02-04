a234115de98715889af3f0456129906181cf2e20
GitHub Action <action@github.com>
2021-02-04 06:37:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/luanjia/hisgdevtool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/luanjia/hisgdevtool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.luanjia.hisgdevtool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.luanjia.hisgdevtool.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/luanjia/hisgdevtool/services/MyProjectService.kt
+++ src/main/kotlin/com/github/luanjia/hisgdevtool/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.luanjia.hisgdevtool.services
 
+import com.github.luanjia.hisgdevtool.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
